9e59f1db194a1f7b6985c89816e19a2f7f9681c9
Renszo D'Giovanni <renszo.camacho@raona.com>
2023-04-05 11:10:26 +0200

    Add rules

--- Firmenich.InternalSafetyDataSheet.Form/.eslintrc.js
+++ Firmenich.InternalSafetyDataSheet.Form/.eslintrc.js
@@ -1,5 +1,25 @@
-require('@rushstack/eslint-config/patch/modern-module-resolution');
+require("@rushstack/eslint-config/patch/modern-module-resolution");
 module.exports = {
-  extends: ['@microsoft/eslint-config-spfx/lib/profiles/react'],
-  parserOptions: { tsconfigRootDir: __dirname }
+  extends: ["@microsoft/eslint-config-spfx/lib/profiles/react"],
+  parserOptions: { tsconfigRootDir: __dirname },
+  rules: {
+    "@microsoft/spfx/no-async-await": "off",
+    "@typescript-eslint/typedef": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "react/jsx-no-bind": ["error", {
+      ignoreRefs: true,
+      allowArrowFunctions: true,
+      allowFunctions: false,
+      allowBind: false,
+      ignoreDOMComponents: true,
+    }],
+    "quotes": ["error", "double"],
+    "jsx-quotes": ["error", "prefer-double"],
+    "react/jsx-closing-bracket-location": ["error", "line-aligned"],
+    "react/jsx-closing-tag-location": "error",
+    "react/jsx-max-props-per-line": ["error", { maximum: 1, when: "multiline" }],
+    "object-curly-spacing": ["error", "always"],
+    "react/jsx-curly-spacing": ["error", "never"],
+    "semi": [2 ,"always" ],
+  }
 };
