9cd169f9179c8a9b715d7eeab8da90ca82946190
Renszo D'Giovanni <renszo.camacho@raona.com>
2023-04-05 11:13:00 +0200

    IntnernalDataServices's refactor

--- Firmenich.InternalSafetyDataSheet.Form/src/Services/InternalSafetyDataSheetService.ts
+++ Firmenich.InternalSafetyDataSheet.Form/src/Services/InternalSafetyDataSheetService.ts
@@ -1,61 +1,51 @@
-import { IItem, IList, ISiteUser, IWeb, Web} from "@pnp/sp/presets/all";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { IWeb, Web } from "@pnp/sp/presets/all";
 import { ISiteUserInfo } from "@pnp/sp/site-users/types";
-import IInternalSafetyDataSheetItem from "../Interfaces/IIterfaces";
+import { IInternalSafetyDataSheetItem } from "../Interfaces/IIterfaces";
 
-export default class InternalSafetyDataSheetService{
-    private web:IWeb;
+export default class InternalSafetyDataSheetService {
+  private _web: IWeb;
 
-    constructor(url: string){
-        this.web = Web(url);
-    }
+  public constructor(url: string) {
+    this._web = Web(url);
+  }
 
-    public async GetItems(listName: string):Promise<any[]>{  
-        var items : any[] = await this.web.lists.getByTitle(listName).items();
-        return items;       
-    }
+  public async GetItems(listName: string): Promise<any[]> {
+    const items: any[] = await this._web.lists.getByTitle(listName).items();
+    return items;
+  }
 
-    public async GetItemsCurrentList(listGuid: string):Promise<any[]>{  
-        var items : any[] = await this.web.lists.getById(listGuid).items.getAll();
-        return items;       
-    }
-    
-    public async GetItemsById(listName: string, itemId: number):Promise<IItem>{  
-        var item : any = await this.web.lists.getByTitle(listName).items.getById(itemId).get();
-        return item;       
-    }
+  public async GetItemsCurrentList(listGuid: string): Promise<any[]> {
+    const items: any[] = await this._web.lists.getById(listGuid).items.getAll();
+    return items;
+  }
 
+  public async GetItemsById(listName: string, itemId: number): Promise<any> {
+    const item = await this._web.lists.getByTitle(listName).items.getById(itemId).get();
+    return item;
+  }
 
-    public async AddInternalSafetyDataSheetItem(listName: string, internalSafetyDataSheetItem : IInternalSafetyDataSheetItem){
-        try {
-            console.log(internalSafetyDataSheetItem);
-            await this.web.lists.getByTitle(listName).items.add({
-                Title: internalSafetyDataSheetItem.Title,
-                RaonaAcciepand: {
-                    results: [internalSafetyDataSheetItem.RaonaAcciepand]
-                }
-            });
-        } catch (error) {
-            throw error;
-        }        
-    }
+  public async AddInternalSafetyDataSheetItem(listName: string, internalSafetyDataSheetItem: IInternalSafetyDataSheetItem): Promise<void> {
+    await this._web.lists.getByTitle(listName).items.add({
+      Title: internalSafetyDataSheetItem.Title,
+      RaonaAcciepand: {
+        results: [internalSafetyDataSheetItem.RaonaAcciepand]
+      }
+    });
 
-    public async UpdateInternalSafetyDataSheetItem(listName: string, itemId: number , internalSafetyDataSheetItem : IInternalSafetyDataSheetItem){
-        try {
-            console.log(internalSafetyDataSheetItem);
-            await this.web.lists.getByTitle(listName).items.getById(itemId).update({
-                Title: internalSafetyDataSheetItem.Title,
-                RaonaAcciepand: {
-                    results: [internalSafetyDataSheetItem.RaonaAcciepand]
-                }
-            });
-        } catch (error) {
-            throw error;
-        }        
-    }
+  }
+  public async UpdateInternalSafetyDataSheetItem(listName: string, itemId: number, internalSafetyDataSheetItem: IInternalSafetyDataSheetItem): Promise<void> {
+    await this._web.lists.getByTitle(listName).items.getById(itemId).update({
+      Title: internalSafetyDataSheetItem.Title,
+      RaonaAcciepand: {
+        results: [internalSafetyDataSheetItem.RaonaAcciepand]
+      }
+    });
+  }
 
-    public async GetUserMail(userId: number): Promise<string>{
-        var user : ISiteUserInfo = await this.web.getUserById(userId).get();        
-        console.log(user);
-        return user.Email;
-    }
+  public async GetUserMail(userId: number): Promise<string> {
+    const user: ISiteUserInfo = await this._web.getUserById(userId).get();
+    console.log(user);
+    return user.Email;
+  }
 }
