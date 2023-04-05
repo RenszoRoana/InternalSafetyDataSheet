b656b9a5be9930dbd7035a230f07a045035b7a8a
Renszo D'Giovanni <renszo.camacho@raona.com>
2023-04-05 11:15:06 +0200

    Modify FirmenichFormCustomizer

--- Firmenich.InternalSafetyDataSheet.Form/src/extensions/firmenichCustomForm/FirmenichCustomFormFormCustomizer.ts
+++ Firmenich.InternalSafetyDataSheet.Form/src/extensions/firmenichCustomForm/FirmenichCustomFormFormCustomizer.ts
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
-import { Log } from '@microsoft/sp-core-library';
+import { Log } from "@microsoft/sp-core-library";
 import {
   BaseFormCustomizer
-} from '@microsoft/sp-listview-extensibility';
-
-import FirmenichCustomForm, { IFirmenichCustomFormProps } from './components/FirmenichCustomForm';
+} from "@microsoft/sp-listview-extensibility";
+import FirmenichCustomForm from "./components/FirmenichCustomForm";
+import { IFirmenichCustomFormProps } from "./components/IFirmenichCustomForm";
 
 /**
  * If your field customizer uses the ClientSideComponentProperties JSON input,
@@ -18,7 +18,7 @@ export interface IFirmenichCustomFormFormCustomizerProperties {
   sampleText?: string;
 }
 
-const LOG_SOURCE: string = 'FirmenichCustomFormFormCustomizer';
+const LOG_SOURCE: string = "FirmenichCustomFormFormCustomizer";
 
 export default class FirmenichCustomFormFormCustomizer
   extends BaseFormCustomizer<IFirmenichCustomFormFormCustomizerProperties> {
@@ -26,7 +26,7 @@ export default class FirmenichCustomFormFormCustomizer
   public onInit(): Promise<void> {
     // Add your custom initialization to this method. The framework will wait
     // for the returned promise to resolve before rendering the form.
-    Log.info(LOG_SOURCE, 'Activated FirmenichCustomFormFormCustomizer with properties:');
+    Log.info(LOG_SOURCE, "Activated FirmenichCustomFormFormCustomizer with properties:");
     Log.info(LOG_SOURCE, JSON.stringify(this.properties, undefined, 2));
     return Promise.resolve();
   }
@@ -40,7 +40,7 @@ export default class FirmenichCustomFormFormCustomizer
         displayMode: this.displayMode,
         onSave: this._onSave,
         onClose: this._onClose
-       } as IFirmenichCustomFormProps);
+      } as IFirmenichCustomFormProps);
 
     ReactDOM.render(firmenichCustomForm, this.domElement);
   }
@@ -55,10 +55,10 @@ export default class FirmenichCustomFormFormCustomizer
 
     // You MUST call this.formSaved() after you save the form.
     this.formSaved();
-  }
+  };
 
-  private _onClose =  (): void => {
+  private _onClose = (): void => {
     // You MUST call this.formClosed() after you close the form.
     this.formClosed();
-  }
+  };
 }
