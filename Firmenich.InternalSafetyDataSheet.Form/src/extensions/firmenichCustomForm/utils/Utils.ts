23573a68415b346682a9cad93dc6b18da00fc85f
Renszo D'Giovanni <renszo.camacho@raona.com>
2023-04-05 11:14:13 +0200

    Create a class Utils

---
+++ Firmenich.InternalSafetyDataSheet.Form/src/extensions/firmenichCustomForm/utils/Utils.ts
@@ -0,0 +1,30 @@
+import { DisplayMode } from "../components/IFirmenichCustomForm";
+
+export class Utils {
+    public static RemoveSpaces(str: string): string {
+        return str.replace(/\s/g, "");
+    };
+
+    public static FirstLetterToLowerCase(str: string): string {
+        const result = str.charAt(0).toLowerCase() + str.slice(1);
+        return this.RemoveSpaces(result);
+    };
+
+    public static DisplayButtonTextMessage(displayMode: DisplayMode): string {
+        const message = {
+            [DisplayMode.NewMode]: "Save",
+            [DisplayMode.EditMode]: "Save",
+            [DisplayMode.ViewMode]: "Print Preview",
+        };
+        return message[displayMode] || "";
+    }
+
+    public static DisplayFormTitle(displayMode: number): string {
+        const message = {
+            [DisplayMode.NewMode]: "New Product",
+            [DisplayMode.EditMode]: "Edit Product",
+            [DisplayMode.ViewMode]: "Preview Product",
+        };
+        return message[displayMode] || "";
+    }
+}
